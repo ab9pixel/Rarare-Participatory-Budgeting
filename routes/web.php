609616f4087bab8372eaa6973127a16496d11869
ahsanmirza10 <mirzaahsan465@gmail.com>
2022-09-28 14:34:55 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ $router->get('/', function () use ($router) {
 
 
 $router->group(['prefix'=>'api'],function() use ($router){
-    $router->get('list/{count}/{user_id}/{type}','ParticipatoryBudgetController@list');
+    $router->get('list/{count}/{user_id}/{type}/{isHome}','ParticipatoryBudgetController@list');
     $router->get('find/{id}','ParticipatoryBudgetController@find');
     $router->get('delete/{id}','ParticipatoryBudgetController@delete');
 
